14f1aeda8ed0c7141bea4c9df5c8450d85ded669
Matty-Coding <mattyloffry.01@gmail.com>
2026-08-11 20:08:53 +0200

    env file configuration changed

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 from pydantic import EmailStr
 
@@ -43,11 +43,11 @@ class Settings(BaseSettings):
     ]
 
     # models
-    model_config = {
-        "env_file": Path(__file__).parent.parent.parent / ".env",
-        "env_file_encoding": "utf-8",
-        "extra": "ignore",
-    }
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent.parent.parent / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+    )
 
 
 settings = Settings()
