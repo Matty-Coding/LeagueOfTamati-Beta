07b584931f2359d053c7ea02c974571a8d21d073
Matty-Coding <mattyloffry.01@gmail.com>
2026-08-11 20:12:39 +0200

    removed ssl db connnection as required

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, connect_args={"ssl": True})
+engine = create_async_engine(settings.DATABASE_URL)
 
 AsyncSessionLocal = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False)
